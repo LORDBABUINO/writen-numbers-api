c87fb9240778d2ef160d5ca1043867d3a5355a18
LORDBABUINO <jorge.x7@gmail.com>
2020-12-24 04:09:41 +0000

    test(unit): replace unit test
    The unit test example actually was an integration test, so it was replaced by a true unit test
    example.

--- __tests__/start/app.unit.test.ts
+++ __tests__/start/app.unit.test.ts
@@ -1,11 +1,5 @@
-import request from 'supertest'
-
-import app from '../../src/start/app'
-
-describe('Route testing', () => {
-  it('Should return an http 200 and a "message" property (route: GET )', async () => {
-    const res = await request(app).get('/')
-    expect(res.status).toEqual(200)
-    expect(res.body).toHaveProperty('message')
+describe('simple test', () => {
+  it('Should return true', async () => {
+    expect(true).toBeTruthy()
   })
 })
