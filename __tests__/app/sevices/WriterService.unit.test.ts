d727932b8d5570adcf56f0b384187e0e4bf29a3c
LORDBABUINO <jorge.x7@gmail.com>
2021-02-16 12:23:02 -0300

    refactor: extract dictionary with writen number and test cases

--- __tests__/app/sevices/WriterService.unit.test.ts
+++ __tests__/app/sevices/WriterService.unit.test.ts
@@ -1,54 +1,10 @@
 import WriterService from '../../../src/app/services/WriterService'
+import testCaseList from '../TestCaseList'
 
 describe('WriterService', () => {
-  interface Dictionary {
-    [number: string]: string
-  }
-
-  const getDictionary = (): Dictionary => ({
-    0: 'zero',
-    1: 'um',
-    2: 'dois',
-    3: 'três',
-    4: 'quatro',
-    5: 'cinco',
-    6: 'seis',
-    7: 'sete',
-    8: 'oito',
-    9: 'nove',
-    10: 'dez',
-    11: 'onze',
-    12: 'doze',
-    13: 'treze',
-    14: 'catorze',
-    15: 'quinze',
-    16: 'dezesseis',
-    17: 'dezessete',
-    18: 'dezoito',
-    19: 'dezenove',
-    20: 'vinte',
-    30: 'trinta',
-    40: 'quarenta',
-    50: 'cinquenta',
-    60: 'sessenta',
-    70: 'setenta',
-    80: 'oitenta',
-    90: 'noventa',
-    100: 'cem',
-    200: 'duzentos',
-    300: 'trezentos',
-    400: 'quatrocentos',
-    500: 'quinhentos',
-    600: 'seiscentos',
-    700: 'setecentos',
-    800: 'oitocentos',
-    900: 'novecentos',
-    1000: 'mil',
-  })
-
-  Object.keys(getDictionary()).forEach((key) => {
+  Object.keys(testCaseList).forEach((key) => {
     it(`Should should write number ${key}`, async () => {
-      expect(WriterService.translate(Number(key))).toBe(getDictionary()[key])
+      expect(WriterService.translate(Number(key))).toBe(testCaseList[key])
     })
   })
 })
