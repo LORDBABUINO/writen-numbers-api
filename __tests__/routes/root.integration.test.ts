bb5bc605dab285a7bf3697a7013b799a9993e548
LORDBABUINO <jorge.x7@gmail.com>
2021-02-17 18:26:37 -0300

    feat: remove root route

--- __tests__/routes/root.integration.test.ts
+++ __tests__/routes/root.integration.test.ts
@@ -4,14 +4,8 @@ import { Dictionary } from '../../src/app/Dictionary'
 import app from '../../src/start/app'
 
 describe('GET /', () => {
-  it('should connect', async () => {
-    return request(app)
-      .get('/')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then((res: Response) => {
-        expect(res.body).toHaveProperty('message', 'Hello world!')
-      })
+  it('should reject requests without params', async () => {
+    return request(app).get('/').expect(404)
   })
 
   it('should deny favicon.ico request', async () => {
--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,6 @@ import WriterController from './app/controllers/WriterController'
 
 const router = Router()
 
-router.get('/', (_, res) => {
-  res.json({ message: 'Hello world!' })
-})
-
 router.get('/favicon.ico', (_, res) => res.status(204).end())
 router.get('/:number', WriterController.index)
 
